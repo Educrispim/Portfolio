d710c8bbe22b482341b3aa854ed91b7e29f6830b
Educrispim <educrispim@edu.unifil.br>
2026-06-23 00:27:00 -0300

    Desenvolvimento do portfolio

---
+++ script.js
@@ -0,0 +1,37 @@
+const elementoTexto = document.getElementById('typing-text');
+const palavras = ["Dev?", "GameDev?", "Entusiasta"];
+let indicePalavra = 0;
+let indiceCaractere = 0;
+let apagando = false;
+
+function digitar() {
+    const palavraAtual = palavras[indicePalavra];
+    
+    if (apagando) {
+        elementoTexto.textContent = palavraAtual.substring(0, indiceCaractere - 1);
+        indiceCaractere--;
+    } else {
+        elementoTexto.textContent = palavraAtual.substring(0, indiceCaractere + 1);
+        indiceCaractere++;
+    }
+
+    let velocidadeDigitacao = apagando ? 100 : 200;
+
+    if (!apagando && indiceCaractere === palavraAtual.length) {
+        velocidadeDigitacao = 2500; // Tempo de pausa com a palavra completa
+        apagando = true;
+    } else if (apagando && indiceCaractere === 0) {
+        apagando = false;
+        indicePalavra++;
+        if (indicePalavra >= palavras.length) {
+            indicePalavra = 0;
+        }
+        velocidadeDigitacao = 500; // Tempo antes de começar a digitar a próxima
+    }
+
+    setTimeout(digitar, velocidadeDigitacao);
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(digitar, 1000);
+});
